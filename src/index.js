6764483cdb2f381e6c09086718aaaeeccfefa95b
Vishal Manghnani <vishal-lollers@hotmail.com>
2020-04-27 18:22:41 +0800

    Removed Amplify Dependencies

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
